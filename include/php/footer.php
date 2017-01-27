8d7e9bd4317d7e2777a1201ed117344ff78e484d
vaibhavsingh97 <vaibhav.singh.14cse@bml.edu.in>
2017-01-27 18:28:21 +0530

    Added analytics

--- include/php/footer.php
+++ include/php/footer.php
@@ -37,17 +37,16 @@
 <script type="text/javascript" src="js/imagesloaded.pkgd.min.js"></script>
 <script type="text/javascript" src="js/masonry.pkgd.min.js"></script>
 <!-- <script type="text/javascript" src="js/plugins.min.js"></script> -->
-<script type="javascript">
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-75079553-1', 'auto');
-ga('send', 'pageview');
+  ga('create', 'UA-75079553-2', 'auto');
+  ga('send', 'pageview');
 
 </script>
-
 </body>
 
 </html>
